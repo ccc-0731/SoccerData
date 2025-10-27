a4ab80342def226254ad67acdd297a1c99659744
ccc-0731 <cccpanda0731@gmail.com>
2025-10-27 18:10:05 -0400

    trying to import files

--- importData.py
+++ importData.py
@@ -1,6 +1,13 @@
 import pandas as pd
+import json
 
-url = "https://raw.githubusercontent.com/SkillCorner/opendata/refs/heads/master/data/matches/2015213/2015213_phases_of_play.csv"
-df = pd.read_csv(url)
+url1 = "https://raw.githubusercontent.com/SkillCorner/opendata/refs/heads/master/data/matches/2015213/2015213_phases_of_play.csv"
+phasesOfPlay = pd.read_csv(url1)
 
-print(df.head())
+url2 = "https://raw.githubusercontent.com/SkillCorner/opendata/refs/heads/master/data/matches/2015213/2015213_dynamic_events.csv"
+dynamicEvents = pd.read_csv(url2)
+
+print(phasesOfPlay.describe())
+print(dynamicEvents.describe())
+
+
